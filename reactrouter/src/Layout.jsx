25e1997a8289484470e242d66add5b0721f6e59e
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-19 10:07:16 +0545

    layout

--- reactrouter/src/Layout.jsx
+++ reactrouter/src/Layout.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function Layout() {
   return (
