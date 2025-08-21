976b5b0d9ace31c563ea498e5cedb45c11232cfd
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-21 22:28:22 +0545

    main

--- reactrouter/src/main.jsx
+++ reactrouter/src/main.jsx
@@ -7,9 +7,12 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github from './components/Github/Github.jsx'
 
 
-const router=createBrowserRouter(
+
+  /*
  [
   {
     path: '/',
@@ -26,12 +29,29 @@ const router=createBrowserRouter(
       {
         path: "contact",
         element:<Contact />
-      }
+      },
+      
 
     ]
   }
  ]
-  
+ */
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:id' element={<User />} />
+      <Route 
+      path='github'
+      element={<Github />} 
+      
+      />
+      
+    </Route>
+  )
 )
 createRoot(document.getElementById('root')).render(
   <StrictMode>                         
