f946723af411544406aaab70618ccd759244c9f9
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-21 20:46:11 +0545

    usercomponent

--- reactrouter/src/components/User/User.jsx
+++ reactrouter/src/components/User/User.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import { useParams } from 'react-router-dom'; // Import useParams to access URL parameters
 function User() {
+  const {id} = useParams(); // useParams hook to access the dynamic segment of the URL
+  // id will contain the value of the dynamic segment in the URL, e.g., if
   return (
-    <div>User</div>
+    <div className='bg-cyan-600 p-4 text-black text-3xl'>User:{id}</div>
   )
 }
 
