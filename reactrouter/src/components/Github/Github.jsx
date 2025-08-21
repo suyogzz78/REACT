0cd900d5faa2d608b44b29b7f3629416eaa2265d
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-21 22:26:12 +0545

    Gitcompo

--- reactrouter/src/components/Github/Github.jsx
+++ reactrouter/src/components/Github/Github.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
 
 function Github() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('https://api.github.com/users/suyogzz78')
+    .then(response => response.json() )
+    .then(data => setData(data))
+  }, [])
   return (
-    <div>Github</div>
+    <>
+    <div className='p-4 text-3xl bg-cyan-900 text-white text-center'>Github Followers :{data.followers}
+    <img src= {data.avatar_url} alt='git picture' width={300}/>
+    </div>
+     </>
   )
 }
-
+ 
 export default Github
