5d56c49fffc70f587019b786d4cdf8319f424ead
suyogzz78 <sakurachhetri97@gmail.com>
2025-08-22 07:21:13 +0545

    GIthubupdate

--- reactrouter/src/components/Github/Github.jsx
+++ reactrouter/src/components/Github/Github.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect ,useState} from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
+  /*
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch('https://api.github.com/users/suyogzz78')
     .then(response => response.json() )
     .then(data => setData(data))
   }, [])
+  */
+ const data =useLoaderData();
   return (
     <>
     <div className='p-4 text-3xl bg-cyan-900 text-white text-center'>Github Followers :{data.followers}
@@ -17,3 +20,8 @@ function Github() {
 }
  
 export default Github
+
+export const githubLoader = async () => {
+  const response = await fetch('https://api.github.com/users/suyogzz78');
+  return response.json();
+}
