6ecef015642eb815ce724998754844ac8f375ffc
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-23 22:04:28 +0545

    update

---
+++ minicontext/src/Context/UserContextProvider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Usercontext from './Usercontext'
+
+const UserContextProvider=({children})=>{
+    const [user,setUser]=React.useState(null)   
+    return(
+        <Usercontext.Provider value={{user,setUser}}>
+            {children}
+        </Usercontext.Provider>
+    )
+}
+export default UserContextProvider
