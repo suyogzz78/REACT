4bd922f43deddf0407b898a1782c5330a0676d69
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-23 22:09:53 +0545

    update

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Context api</h1>
-     
-    </>
+    <UserContextProvider>
+    
+     <h1>Suyog donnn</h1>
+    </ UserContextProvider>
   )
 }
 
