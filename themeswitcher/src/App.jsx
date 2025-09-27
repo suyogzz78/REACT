e54126899ace5804ef35d35157b0992b65f378af
Suyog Gautam <sakurachhetri97@gmail.com>
2025-09-27 20:10:25 +0545

    update

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <div className='text-white p-7 bg-red-500'>SUYOG GAUTAM
-      </div>
+   
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* Theme switcher button */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                        {/* card ui*/}
+                    </div>
+                </div>
+            </div>
+
       
   )
 }
