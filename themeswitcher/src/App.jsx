6eb73aa14707c40b493e96e7a216a3a71d758b10
Suyog Gautam <sakurachhetri97@gmail.com>
2025-10-09 14:09:59 +0545

    update

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -2,18 +2,19 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
    
+
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* Theme switcher button */}
+                        {/*cthemebtn*/}
                     </div>
 
-                    <div className="w-full max-w-sm mx-auto mb-4">
-                        {/* card uii*/}
+                    <div className="w-full max-w-sm mx-auto">
+                       {/*card*/}
                     </div>
                 </div>
             </div>
