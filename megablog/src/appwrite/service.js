f96d4cccaba065284687886dd2872915966c35fd
Suyog Gautam <sakurachhetri97@gmail.com>
2025-10-26 23:24:59 +0545

    addding create

--- megablog/src/appwrite/service.js
+++ megablog/src/appwrite/service.js
@@ -3,8 +3,32 @@ import { Client, Account, Databases, Storage ,Query } from "appwrite";
 
 export class AppwriteService {
     client = new Client();
-    account;
+    bucket;
     databases;
+    constructor() {
+        this.client
+            .setEndpoint(config.appwriteurl)
+            .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+    async createPost({title,content,slug,featuredImage,status,userId }) {
+        try{
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug,
+                {title,
+                content,
+                featuredImage,
+                status,
+                userId}
+            );
+        } catch (error) {
+            throw error;
+        }
+
+    }
 }
 const service = new AppwriteService();
  export default service;
