424788f984adfa12ed0e08032a9fa7164ac8eaa3
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-07 23:53:55 +0545

    use of dispatch

--- megablog/src/App.jsx
+++ megablog/src/App.jsx
@@ -1,17 +1,48 @@
-import { useState,useEffect } from 'react'
-
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authslice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
-  return (
-  <>
-  <h1 >blog app using appwrite</h1>
 
-  </>
-  )
+  useEffect(() => { 
+    async function fetchUser() {
+      try {
+        const userdata = await authService.getCurrentUser();
+        if (userdata) {
+          dispatch(login({ userdata }));
+        } else {
+          dispatch(logout());
+        }
+      } catch (err) {
+        console.error("Appwrite getCurrentUser error:", err);
+        dispatch(logout());
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchUser();
+  }, [dispatch]);
+
+  if (loading) return null;
+
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-500">
+      <Header />
+      <main className="flex-1 flex items-center justify-center">
+        <Outlet />
+      </main>
+      <Footer />
+    
+
+    </div>
+ 
+);
 }
 
-export default App
+export default App;
