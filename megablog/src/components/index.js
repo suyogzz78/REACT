70a4bf89f1e930a37e53c09e9c56ab68b131d550
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-10 15:05:59 +0545

    index

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Logo from "./Logo";
+import Container from "../container/Container";
+import Logout from "./Header/Logout";
     export {
         Header,
-        Footer
+        Footer,
+        Container,
+        Logo,
+        Logout
     }
