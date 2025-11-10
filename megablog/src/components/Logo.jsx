08a1cda5b005a76bca818b09945b4f9922713ef5
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-10 15:07:35 +0545

    logofortheblog

--- megablog/src/components/Logo.jsx
+++ megablog/src/components/Logo.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Logo() {
     return (
-        <div>Logo</div>
+        <div>
+        <img src='https://images.pexels.com/photos/2180780/pexels-photo-2180780.jpeg' style ={{width:"200px",height:"150px",borderRadius:"50%"}}></img>
+        </div>
     )
 }
 
