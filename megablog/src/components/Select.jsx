562dd6ee1209f4f01a8f5f15ee6a54ddbef72adc
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-12 15:36:11 +0545

    selectcompocompleted

--- megablog/src/components/Select.jsx
+++ megablog/src/components/Select.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
-function Select() {
+function Select({
+    options,
+    label,
+    className = "",
+    ...props
+}) {
+    const id = React.useId(); //generating unique id for select field
     return (
-        <></>
+       <div className='w-full'>
+        {label && <label htmlFor={id} className='block
+             text-gray-700 font-bold mb-2'>{label}</label>}
+        <select
+        {...props}
+        id={id}
+        className={`px-3 py-2 border border-gray-300 rounded-lg w-full focus:outline-none ${className}`}
+        >
+            {options?.map((option) => (
+                <option key={option} value={option}>
+                    {option}
+                </option>       
+            )
+            )
+        }
+            
+        </select>
+       </div>
     )
 }
 
-export default Select
+export default React.forwardRef(Select)
