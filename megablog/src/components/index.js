52da67b2ecb57bcfdaf2959c17b9693de9da0c4f
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-13 20:42:16 +0545

    updateoncodeorg

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import Container from "../container/Container";
 import Logout from "./Header/Logout";
+import Card from "./Card";
     export {
         Header,
         Footer,
         Container,
         Logo,
-        Logout
+        Logout,
+        Card
     }
