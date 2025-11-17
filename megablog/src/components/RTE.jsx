f3463a2ed5a1e1064194bcd5c0932f327496b795
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-17 13:40:59 +0545

    updatesonrichtexteditor

--- megablog/src/components/RTE.jsx
+++ megablog/src/components/RTE.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
-import { Controller } from 'react-hook-form'
+import React from "react";
+import { Controller } from "react-hook-form";
+import { Editor } from "@tinymce/tinymce-react";
 
-export default function RTE({name,control,label,defaultValue=""}) {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <div className='w-full'>
-      {label && <label className='inline-block mb-1 p-2'>{label}</label>}
-      <Controller
-      name='name'
-      
-      />
-      
-    </div>
-  )        
-}
+    <div className="w-full">
+      {label && <label className="inline-block mb-1 p-2">{label}</label>}
 
+      <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field: { onChange, value } }) => (
+          <Editor
+            apiKey="no-api-key"
+            value={value}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "searchreplace",
+                "visualblocks",
+                "code",
+                "fullscreen",
+                "insertdatetime",
+                "media",
+                "table",
+                "help",
+                "wordcount",
+              ],
+              toolbar:
+                "undo redo | blocks | image | " +
+                "bold italic forecolor | alignleft aligncenter " +
+                "alignright alignjustify | bullist numlist outdent indent | " +
+                "removeformat | help",
+              content_style:
+                "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
+}
