06399501a8fbdb736914c63e4727b75e00f9e7f1
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-17 22:55:10 +0545

    codemanagement

--- megablog/src/components/index.js
+++ megablog/src/components/index.js
@@ -4,11 +4,13 @@ import Logo from "./Logo";
 import Container from "../container/Container";
 import Logout from "./Header/Logout";
 import Card from "./Card";
+import RTE from "./RTE";
     export {
         Header,
         Footer,
         Container,
         Logo,
         Logout,
-        Card
+        Card,
+        RTE
     }
