036b7e8e097305a756137db2e37070eb1f31a0ee
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-20 22:27:26 +0545

    allpostpage

---
+++ megablog/src/Pages/AllPost.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import { AppwriteService } from '../appwrite/service';
+import { Container, Card } from '../components';
+
+function AllPost() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    AppwriteService.getPosts().then((posts) => {
+      if (result) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
+  return (
+    <div className='py-7 w-full'>
+      <Container>
+        <div className='flex flex-wrap'>
+          {posts.map((post) => (
+            <div key={post.$id} className='px-2'>
+              <Card post={post} />
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default AllPost;
