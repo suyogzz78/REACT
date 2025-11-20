78e0d563cbff598cd81ab903dee1928526b36319
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-20 20:31:00 +0545

    finalupdate

--- megablog/src/components/Button.jsx
+++ megablog/src/components/Button.jsx
@@ -10,7 +10,10 @@ function Button({children,
     return (
         <button
         className={`px-4 py-3 rounded-xl  ${bgColor} ${textColor} ${className}`} {...props} >    {children}      </button>
-        
+        //here bgColor and textColor are expected to be tailwind css classes like bg-blue-500 or text-white and the spread operator
+        //  ...props allows passing any other button attributes like onClick, disabled etc.
+        //children represents the content inside the button tags
+        //the className prop allows for additional custom styling to be added
     )
 }
 
