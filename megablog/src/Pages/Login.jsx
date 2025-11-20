a10d800a9acacccb6634645e6c5d753e1d6552b0
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-20 22:27:59 +0545

    newloginpage

---
+++ megablog/src/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Login as Logincompo } from '../components'
+function Login() {
+  return (
+    <div className='py-7'>
+        <Logincompo/>
+    </div>
+  )
+}
+
+export default Login
