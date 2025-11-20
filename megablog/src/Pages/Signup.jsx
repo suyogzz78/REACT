fe7c009d50b58dddd7cbc1781b6fdf8063165148
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-20 22:28:37 +0545

    signuppage

---
+++ megablog/src/Pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup as Signupcompo } from '../components'
+function Signup() {
+  return (
+    <div className='py-7'>
+        <Signupcompo/>
+    </div>
+  )
+}
+
+export default Signup
