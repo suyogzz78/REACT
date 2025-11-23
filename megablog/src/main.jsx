35415199c8c6545e5d096c0c2b80567a04a242c4
Suyog Gautam <sakurachhetri97@gmail.com>
2025-11-23 23:30:55 +0545

    routingcompleted

--- megablog/src/main.jsx
+++ megablog/src/main.jsx
@@ -4,14 +4,81 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import  store  from './store/store.js'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom'
+import { Authlayout } from './components/index.js'
+import AllPost from './Pages/AllPost.jsx'
+import AddPost from './Pages/AddPost.jsx'
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>,
+    children:[
+      {
+          path:'/',
+          element:<Home/>
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+      },
+      {
+        path:'/login',
+        element:(
+          <Authlayout authentication={false}>
+              <Login/>
+          </Authlayout>
+        )
+      },
+      {
+        path:'/signup',
+        element:(
+          <Authlayout authentication={false}>
+            <Signup/>
+            
+          </Authlayout>
+        )
+      },
+      {
+        path:'/all-posts',
+        element:(
+          <Authlayout authentication>
+            {""}
+            <AllPost/>
+
+          </Authlayout>
+        )
+      },
+      {
+        path:'/edit-post/:slug',
+        element:(
+          <Authlayout authentication>
+            {""}
+            <Editpost/>
+          </Authlayout>
+        )
+      },
+      {
+        path:'/add-post',
+        element:(
+          <Authlayout>
+            {""}
+            <AddPost/>
+          </Authlayout>
+        )
+      },
+      {
+        path:'/post/:slug',
+        element:<Post/>
+
+      }
+
+    ]
+    
+  }
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </Provider>
-  </StrictMode>,
+    <RouterProvider router={router}/>
+    </Provider>
+  </React.StrictMode>,
 )
