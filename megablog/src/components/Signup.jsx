1c5911aefcfe949d82965469937b1fee6c85d68c
Suyog Gautam <sakurachhetri97@gmail.com>
2025-12-22 17:48:29 +0545

    update

--- megablog/src/components/Signup.jsx
+++ megablog/src/components/Signup.jsx
@@ -1,88 +1,88 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { AppwriteService } from "../appwrite/service.js";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Button, Input, Logo } from "./index.js";
-import { useForm } from "react-hook-form";
+import React, {useState} from 'react'
+import authService from '../appwrite/auth'
+import {Link ,useNavigate} from 'react-router-dom'
+import {login} from '../store/authSlice'
+import {Button, Input, Logo} from './index.js'
+import {useDispatch} from 'react-redux'
+import {useForm} from 'react-hook-form'
 
 function Signup() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const { register, handleSubmit } = useForm();
-  const [error, setError] = React.useState("");
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
 
-  const signup = async (data) => {
-    //async function is used to handle asynchronous operations like API calls
-    setError("");
-    try {
-      const userdata = await AppwriteService.createAccount(data); //data contains email,password,name
-      if (userdata) {
-        const userdata = await AppwriteService.getCurrentUser();
-        if (userdata) {
-          dispatch(login({ userdata })); //login is action creator imported from authslice.js to update redux store with user data
-          navigate("/"); //navigating to home page after successful signup
+    const create = async(data) => {
+        setError("")
+        try {
+            const userData = await authService.createAccount(data)
+            if (userData) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(login(userData));
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
         }
-      }
-    } catch (error) {
-      setError(error.message);
     }
-  };
 
   return (
-    <div className="flex items-center justify-center w-full">
-      <div className="w-full max-w-md p-8 border border-gray-300 rounded-lg shadow-lg mx-auto">
-        <div className="flex justify-center mb-6">
-          <span className="inline-block w-full max-w-[100px]">
-            <Logo width="150px" />
-          </span>
-        </div>
-        <h2 className="text-2xl font-bold mb-6 text-center">
-          SignIn to Your Account
-        </h2>
-        <p className="mt-2 text-center text-base text-black/60">
-          Don&apos;t have any account?&nbsp;
-          <Link
-            to="/login"
-            className="font-medium text-primary transition-all duration-200 hover:underline"
-          >
-            Sign In
-          </Link>
-        </p>
-        {error && <p className="text-red-500 mb-4">{error}</p>}
+    <div className="flex items-center justify-center">
+            <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <div className="mb-2 flex justify-center">
+                    <span className="inline-block w-full max-w-[100px]">
+                        <Logo width="100%" />
+                    </span>
+                </div>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+                <p className="mt-2 text-center text-base text-black/60">
+                    Already have an account?&nbsp;
+                    <Link
+                        to="/login"
+                        className="font-medium text-primary transition-all duration-200 hover:underline"
+                    >
+                        Sign In
+                    </Link>
+                </p>
+                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
 
-        <form onSubmit={handleSubmit(signup)} className="mt-8">
-          <div className="space-y-4 ">
-            <Input
-              label="Name"
-              type="text"
-              placeholder="Enter your name"
-              {...register("name", { required: true })}
-            />
-            <Input
-              label="Email"
-              type="email"
-              placeholder="Enter your email"
-              {...register("email", {
-                required: true,
-                validate: (value) =>
-                  /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/gim.test(value) ||
-                  "Invalid email address",
-              })} //here ...register is used to register the input field with react-hook-form and it takes the field name and validation rules as arguments
-            />
+                <form onSubmit={handleSubmit(create)}>
+                    <div className='space-y-5'>
+                        <Input
+                        label="Full Name: "
+                        placeholder="Enter your full name"
+                        {...register("name", {
+                            required: true,
+                        })}
+                        />
+                        <Input
+                        label="Email: "
+                        placeholder="Enter your email"
+                        type="email"
+                        {...register("email", {
+                            required: true,
+                            validate: {
+                                matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                "Email address must be a valid address",
+                            }
+                        })}
+                        />
+                        <Input
+                        label="Password: "
+                        type="password"
+                        placeholder="Enter your password"
+                        {...register("password", {
+                            required: true,})}
+                        />
+                        <Button type="submit" className="w-full">
+                            Create Account
+                        </Button>
+                    </div>
+                </form>
+            </div>
 
-            <Input
-              type="password"
-              label="Password"
-              placeholder="Enter your password"
-              {...register("password", { required: true })}
-            />
-            <Button type="submit">Create Account</Button>
-          </div>
-        </form>
-      </div>
     </div>
-  );
+  )
 }
 
-export default Signup;
+export default Signup
