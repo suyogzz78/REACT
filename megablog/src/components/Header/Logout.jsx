ae867c87c94c2eba053648601f616a86e271e817
Suyog Gautam <sakurachhetri97@gmail.com>
2025-12-22 17:47:07 +0545

    update

--- megablog/src/components/Header/Logout.jsx
+++ megablog/src/components/Header/Logout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import authService from '../../appwrite/auth'
-import {logout} from '../../store/authslice.js'
+import {logout} from '../../store/authSlice.js'
 import { use } from 'react'
 function Logout() {
 
